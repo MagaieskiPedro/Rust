9a4f70082095b74b566defb712e9f8a1094758a2
magaieskipedro <magaieskipedro@gmail.com>
2025-08-01 08:43:50 -0300

    variaveis e constantes

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+	println!("Descubra o numero:");	    
+	let mut tentativa = String::new();
+
+	io::stdin()
+		.read_line(&mut tentativa)
+		.expect("Erro ao ler dado");
+	println!("Você chutou: {tentativa}");
+	
+	let x = 5;
+	let y = 10;
+	println!("x = {x} and y + 2 = {}",y + 2);
 }
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/susie/projects/guessing_game/target/debug/guessing_game: /home/susie/projects/guessing_game/src/main.rs
