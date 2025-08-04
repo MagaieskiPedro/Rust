8c40e8ed9fe28b65fcd5dce5da9766bea2250b86
MagaieskiPedro <magaieskipedro@gmail.com>
2025-08-04 11:19:34 -0300

    comparação

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,23 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-	println!("Descubra o numero:");	    
 	let mut tentativa = String::new();
+	let numero_secreto = ran::thread_rng().gen_range(1..=100)
+	println!("Descubra o numero:");	    
 
 	io::stdin()
 		.read_line(&mut tentativa)
 		.expect("Erro ao ler dado");
 	println!("Você chutou: {tentativa}");
+
+	match tentativa.cmp(&numero_secreto) {
+		Ordering::Less => println!("Muito baixo"),
+		Ordering::Greater => println!("Muito alto"),
+		Ordering::Equal => println!("Acertou"),
 	
-	let x = 5;
-	let y = 10;
-	println!("x = {x} and y + 2 = {}",y + 2);
+//	let x = 5;
+//	let y = 10;
+//	println!("x = {x} and y + 2 = {}",y + 2);
 }
